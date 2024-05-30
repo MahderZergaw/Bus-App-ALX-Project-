30fb2249f44f9f928a4db8ed30f276f6214b5172
Madu-f0r0 <duherbert@gmail.com>
2024-05-30 19:23:36 +0100

    Added more data entries to the database

--- Busbackend/Bus/models.py
+++ Busbackend/Bus/models.py
@@ -11,10 +11,16 @@ class Bus(models.Model):
     destination = models.CharField(max_length=255)
     price = models.FloatField()
 
+    def __str__(self):
+        return f"{self.platenumber}: {self.start_location} - {self.destination}"
+
 
 class Seat(models.Model):
     bus = models.ForeignKey(Bus, related_name='seats', on_delete=models.CASCADE)
     seat_number = models.CharField(max_length=5)
+
+    def __str__(self):
+        return f"{self.bus.platenumber} ({self.seat_number})"
 
 
 class Schedule(models.Model):
@@ -22,11 +28,17 @@ class Schedule(models.Model):
     departure_time = models.DateTimeField()
     arrival_time = models.DateTimeField()
 
+    def __str__(self):
+        return f"({self.bus.platenumber}) {self.bus.start_location} - {self.bus.destination} ({self.departure_time} - {self.arrival_time})"
+
 
 class ScheduleSeat(models.Model):
     schedule = models.ForeignKey(Schedule, related_name='schedule_seats', on_delete=models.CASCADE)
     seat = models.ForeignKey(Seat, related_name='schedule_seats', on_delete=models.CASCADE)
     is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.schedule} Seat {self.seat.seat_number}"
 
 
 class Booking(models.Model):
@@ -41,6 +53,9 @@ class Booking(models.Model):
 class Terminal(models.Model):
     name = models.CharField(max_length=255)
     address = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class WaitingList(models.Model):
