c31449e64dd704031f8e58992b12e263e25f6503
MahderZergaw <mahderzergaw640@gmail.com>
2024-11-27 09:10:48 +0300

    fresh start for the portfolio enhancement

---
+++ Bus App website/src/constants/routes.js
@@ -0,0 +1,11 @@
+const ROUTES = {
+    home: "/",
+    about: "/#about",
+    login: "/login",
+    registerDriver: "/RegisterDriver",
+    registerUser: "/RegisterUser",
+    logout: "/login", // Add appropriate logout route
+  };
+  
+  export default ROUTES;
+  
